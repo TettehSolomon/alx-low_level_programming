31567d35b0633431454e77eb0182cd5b3fb7b195
TettehSolomon <tettehsolomon106@gmail>
2022-04-12 05:43:32 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,6 +6,7 @@
 void times_table(void)
 {
 	int i, j, k;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
@@ -24,11 +25,12 @@ void times_table(void)
 			}
 			else if (k >= 10)
 			{
-				_putchar(',')
+				_putchar(',');
 				_putchar(' ');
 				_putchar(k / 10 + '0');
 				_putchar(k % 10 + '0');
 			}
 		}
+		_putchar('\n');
 	}
 }
